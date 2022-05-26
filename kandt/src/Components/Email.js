9598eff2c1008ec551c43f10f00955a187384f14
Denilson Granados <denilsongranados4@gmail.com>
2022-05-25 23:14:35 -0400

    Send emails from forms

--- kandt/src/Components/Email.js
+++ kandt/src/Components/Email.js
@@ -1,13 +1,47 @@
 import React from 'react';
-import { Form, Container} from "react-bootstrap"
+import { Form, Container,Button,Modal} from "react-bootstrap";
+import { useState } from 'react';
 function Email() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const handleSubmit =event=>{
+        // https://medium.com/geekculture/how-to-send-emails-from-a-form-in-react-emailjs-6cdd21bb4190
+        setShow(false);
+    }
   return (
-      <Container>
-        <Form>
-            <Form.Label>Your Email</Form.Label>
-            <Form.Control id="email" ></Form.Control>
-        </Form>
-      </Container>
+    <>
+    <Button variant="primary" onClick={handleShow}>
+          Get a Quote!
+    </Button>
+    <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form>
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" id="email"></Form.Control>
+                <Form.Label>Company/Organization</Form.Label>
+                <Form.Control id="org"></Form.Control>
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control id="phone number"></Form.Control>
+                <Form.Label>How Can We Help?</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+            </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      </>
 
   )
 }
